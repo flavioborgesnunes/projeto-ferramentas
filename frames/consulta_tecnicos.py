e89b5d0305eb76a3770f7394183b2b21e1fb8050
Flavio Nunes <flavioborgesnunes@gmail.com>
2022-09-09 19:16:46 -0300

    formatação funções e consulta de tecnicos

--- frames/consulta_tecnicos.py
+++ frames/consulta_tecnicos.py
@@ -1,11 +1,5 @@
-from multiprocessing.sharedctypes import Value
-from optparse import Values
-from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
-from csv import reader
-import csv
-from modulos.funcoes import *
+from tkinter import ttk, Button, Entry, Label, Frame
+from modulos.funcoes import excluir, consultar, limpar, mostrar_tudo
 
 BACKGROUND = '#BBBFCA'
 arquivoCsv = './arquivos/tecnicos.csv'
@@ -14,19 +8,24 @@ arquivoCsv = './arquivos/tecnicos.csv'
 def botao_consultaTec(self):
     # print('consulta Tec')
     self.frame_consultaTec = Frame(
-        self.root, bd=4, bg=BACKGROUND, highlightbackground='#0D0D0D', highlightthickness=0)
+        self.root, bd=4, bg=BACKGROUND, highlightbackground='#0D0D0D',
+        highlightthickness=0)
     self.frame_consultaTec.place(
         relx=0.23, rely=0.0, relwidth=0.77, relheight=1)
 
-    self.title_cunsultaTec = Label(self.frame_consultaTec, fg="#16213E", text="CONSULTA TÉCNICOS", background='#BBBFCA',
+    self.title_cunsultaTec = Label(self.frame_consultaTec, fg="#16213E",
+                                   text="CONSULTA TÉCNICOS",
+                                   background='#BBBFCA',
                                    font=("Arial", 14, "bold"))
     self.title_cunsultaTec.place(relx=0.25, rely=0.02)
 
     self.lb_codigo = Label(self.frame_consultaTec, text='CPF',
-                           background='#BBBFCA', foreground='black', font=("Arial", 12, "bold"))
+                           background='#BBBFCA', foreground='black',
+                           font=("Arial", 12, "bold"))
     self.lb_codigo.place(relx=0.05, rely=0.16)
     self.codigo_entry = Entry(
-        self.frame_consultaTec, highlightbackground='#878787', highlightthickness=1)
+        self.frame_consultaTec, highlightbackground='#878787',
+        highlightthickness=1)
     self.codigo_entry.place(relx=0.05, rely=0.20,
                             relwidth=0.60, relheight=0.04)
 
@@ -54,18 +53,23 @@ def botao_consultaTec(self):
     self.scrollLista.place(relx=0.96, rely=0.3, relwidth=0.04, relheight=0.3)
 
     self.bt_show_all = Button(
-        self.frame_consultaTec, text='Mostrar Tudo', bg='#F2F2F2', command=lambda: mostrar_tudo(self.listaTec, arquivoCsv))
+        self.frame_consultaTec, text='Mostrar Tudo', bg='#F2F2F2',
+        command=lambda: mostrar_tudo(self.listaTec, arquivoCsv))
     self.bt_show_all.place(
         relx=0.06, rely=0.9, relwidth=0.2, relheight=0.05)
     self.bt_limparTec = Button(
-        self.frame_consultaTec, text='Limpar', bg='#F2F2F2', command=lambda: limpar(self.listaTec))
+        self.frame_consultaTec, text='Limpar', bg='#F2F2F2',
+        command=lambda: limpar(self.listaTec))
     self.bt_limparTec.place(
         relx=0.29, rely=0.9, relwidth=0.2, relheight=0.05)
     self.bt_funcaoFer = Button(
-        self.frame_consultaTec, text='Consultar', bg='#F2F2F2', command=lambda: consultar(self.listaTec, self.codigo_entry, arquivoCsv))
+        self.frame_consultaTec, text='Consultar', bg='#F2F2F2',
+        command=lambda: consultar(self.listaTec, self.codigo_entry,
+                                  arquivoCsv))
     self.bt_funcaoFer.place(
         relx=0.52, rely=0.9, relwidth=0.2, relheight=0.05)
     self.bt_excluirTec = Button(
-        self.frame_consultaTec, text='Excluir', bg='#F2F2F2', command=lambda: excluir(self.listaTec, arquivoCsv))
+        self.frame_consultaTec, text='Excluir', bg='#F2F2F2',
+        command=lambda: excluir(self.listaTec, arquivoCsv))
     self.bt_excluirTec.place(
         relx=0.75, rely=0.9, relwidth=0.2, relheight=0.05)
--- modulos/funcoes.py
+++ modulos/funcoes.py
@@ -1,9 +1,9 @@
-from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
+from tkinter import messagebox, END
 from csv import reader
 from csv import writer
 import csv
+
+CAMINHO_SENHA_ENTRADA = './arquivos/senha_entrada.csv'
 
 
 def cadastrar_ferramentas(self):
@@ -26,24 +26,25 @@ def cadastrar_ferramentas(self):
             # Passing the cav_reader object to list() to get a list of lists
             linhas = list(csv_reader)
             codigo = int(linhas[-1][0])
-    except:
+
+    except IndexError:
         codigo = 0
 
-    if (not self.descricao
-        or not self.fabricante
-        or not self.voltagem
-        or (self.voltagem == 'SELECIONE')
-        or not self.tamanho_1
-        or not self.tamanho_2
-        or not self.unidade_medida
-        or not self.tipo_ferramenta
-        or (self.tipo_ferramenta == 'SELECIONE')
-        or not self.material_ferramenta
-        or not self.tempo_reserva
-        or not self.critica
-        or (self.critica == 'SELECIONE')
-        or not self.quantidade
-        ):
+    if not self.descricao \
+       or not self.fabricante \
+       or not self.voltagem \
+       or (self.voltagem == 'SELECIONE') \
+       or not self.tamanho_1 \
+       or not self.tamanho_2 \
+       or not self.unidade_medida \
+       or not self.tipo_ferramenta \
+       or (self.tipo_ferramenta == 'SELECIONE') \
+       or not self.material_ferramenta \
+       or not self.tempo_reserva \
+       or not self.critica \
+       or (self.critica == 'SELECIONE') \
+       or not self.quantidade:
+
         messagebox.showinfo(
             title="Atenção", message='Preencha todos os Campos')
 
@@ -61,12 +62,14 @@ def cadastrar_ferramentas(self):
         self.critica_entry.delete('0', 'end')
         self.quantidade_entry.delete('0', 'end')
         messagebox.showinfo(
-            title="Atenção", message='O código da ferramenta é: ' + str(f'{codigo:04}'))
+            title="Atenção",
+            message='O código da ferramenta é: ' + str(f'{codigo:04}')
+        )
 
         with open('./arquivos/ferramentas.csv', 'a', newline='') as csvfile:
             fieldnames = ['codigo', 'Descricao', 'Fabricante',
                           'Voltagem', 'Tamanho', 'Unidade_de_Medida',
-                          'Tipo_de_Ferramenta', 'Material_da_Ferramenta',
+                          'Tipo', 'Material',
                           'tempo_reserva', 'critica', 'quantidade']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
@@ -77,8 +80,8 @@ def cadastrar_ferramentas(self):
                              'Voltagem': self.voltagem,
                              'Tamanho': self.tamanho,
                              'Unidade_de_Medida': self.unidade_medida.title(),
-                             'Tipo_de_Ferramenta': self.tipo_ferramenta.title(),
-                             'Material_da_Ferramenta': self.material_ferramenta.title(),
+                             'Tipo': self.tipo_ferramenta.title(),
+                             'Material': self.material_ferramenta.title(),
                              'tempo_reserva': self.tempo_reserva,
                              'critica': self.critica,
                              'quantidade': self.quantidade})
@@ -102,17 +105,13 @@ def cadastrar_Tec(self):
 
     self.turno = self.turno_entry.get().title()
     self.equipe = self.equipe_entry.get().title()
-    with open('./arquivos/tecnicos.csv', 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        # Passing the cav_reader object to list() to get a list of lists
-        linhas = list(csv_reader)
 
-    if (not self.cpf
-                or not self.nome
-                or not self.telefone
-                or not self.turno
-                or not self.equipe
-            ):
+    if not self.cpf \
+       or not self.nome \
+       or not self.telefone \
+       or not self.turno \
+       or not self.equipe:
+
         messagebox.showinfo(
             title="Atenção", message='Preencha todos os Campos')
         return
@@ -128,8 +127,15 @@ def cadastrar_Tec(self):
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
             # writer.writeheader()
-            writer.writerow({'cpf': self.cpf, 'nome': self.nome,
-                            'telefone': self.telefone, 'turno': self.turno, 'equipe': self.equipe})
+            writer.writerow(
+                {
+                    'cpf': self.cpf,
+                    'nome': self.nome,
+                    'telefone': self.telefone,
+                    'turno': self.turno,
+                    'equipe': self.equipe
+                }
+            )
         messagebox.showinfo(
             title="Atenção", message='Cadastro realizado com sucesso')
 
@@ -139,7 +145,6 @@ def mostrar_tudo(lista, arquivoCsv):
     with open(arquivoCsv, 'r') as csv_file:
         csv_reader = reader(csv_file)
         linhas = list(csv_reader)
-        elemento_lista = False
         for item in linhas:
             lista.insert("", END, values=item)
 
@@ -162,7 +167,7 @@ def consultar(lista, entrada, arquivoCsv):
                 if item == entrada_codigo:
                     encontrado = True
                     lista.insert("", END, values=definitiva)
-        if encontrado == False:
+        if not encontrado:
             messagebox.showinfo(title="Atenção", message='Não Encontrado')
 
 
@@ -190,7 +195,7 @@ def excluir(lista, arquivoCsv):
                 writer.writerows(lines)
         else:
             return
-    except:
+    except IndexError:
         messagebox.showinfo(
             title="ERRO", message='Selecione um item a ser deletado!')
 
@@ -206,7 +211,7 @@ def cadastrar_login(self):
     self.entry_user_cadastro.delete('0', 'end')
     self.entry_senha_cadastro.delete('0', 'end')
 
-    with open('./arquivos/senha_entrada.csv', 'r') as file:
+    with open(CAMINHO_SENHA_ENTRADA, 'r') as file:
         csv_reader = reader(file)
         linhas = str(csv_reader)
         msg3 = False
@@ -218,7 +223,7 @@ def cadastrar_login(self):
                 title="Atenção", message='Usuário já cadastrado')
 
         else:
-            with open('./arquivos/senha_entrada.csv', 'a', newline='') as csvfile2:
+            with open(CAMINHO_SENHA_ENTRADA, 'a', newline='') as csvfile2:
                 writer_object = writer(csvfile2)
                 list_data = [self.user_cadastro, str(self.senha_cadastro)]
                 writer_object.writerow(list_data)
@@ -243,7 +248,7 @@ def excluir_user(self):
         lines = list()
         compara = [self.user_cadastro, self.senha_cadastro]
         verifica_cadastro = False
-        with open('./arquivos/senha_entrada.csv', 'r') as file:
+        with open(CAMINHO_SENHA_ENTRADA, 'r') as file:
             csv_reader = csv.reader(file)
             for lista in csv_reader:
                 lines.append(lista)
@@ -253,10 +258,10 @@ def excluir_user(self):
                     messagebox.showinfo(
                         title='Atenção', message='Usuário Deletado')
 
-        with open('./arquivos/senha_entrada.csv', 'w', newline='') as writeFile:
+        with open(CAMINHO_SENHA_ENTRADA, 'w', newline='') as writeFile:
             writer = csv.writer(writeFile)
             writer.writerows(lines)
-        if verifica_cadastro == False:
+        if not verifica_cadastro:
             messagebox.showinfo(
                 title="Atenção", message='Usuário não cadastrado')
 
